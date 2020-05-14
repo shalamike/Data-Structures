304d6f6e15c002d2a10ce2e64e7373cac3773283
shalamike <mshala02@dcs.bbk.ac.uk>
2020-05-14 12:11:29 +0100

    inor tweeks moving to the linkedlist class moving the push methods out of the static node class to make it more syntactically correct

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -12,32 +12,7 @@ public class LinkedList {
             data = d;
             next = null;
         }
-
-
-        /* given a reference to the head of a list and an int,
-        this new "push" method will insert a new node to the front of the list*/
-        public void push(Node head, int new_data)
-        {
-
-            /* 1. alloc the Node and put data*/
-            Node new_Node = new Node(new_data);
-
-            /* 2. Make next of new Node as head */
-            new_Node.next = head;
-
-            /* 3. Move the head to point to new Node */
-            head = new_Node;
-        }
-
-        //this push method will add a new node as demonstrated below
-        /*
-          new head/Node       previous head/Node     second node  ......    Nth node + 1
-             |                |                       |                      |
-             |                |                       |                      |
-         +----+------+     +----+------+            +----+------+         +------+------+
-         | 1  | null |     | 2  | null |            |  3 | null |         |  n+1 | null |
-         +----+------+     +----+------+            +----+------+  .....  +------+------+
-         */
+        
 
     }
     // creating a method DealWithUnderflow which simply creates a new list with a new node at index 1
