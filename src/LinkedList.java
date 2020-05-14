1d32ba43a786734c38f38f0c0e9cb5ab356abaf1
shalamike <mshala02@dcs.bbk.ac.uk>
2020-05-14 09:18:50 +0100

    added two methods, DealWithOverflow and DealWithUnderflo for use in future operations

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -13,8 +13,17 @@ public class LinkedList {
 
 
         }
-        
 
+        // creating a method DealWithUnderflow wich simply checks if the list is empty will create a new node at index 1
+        Node DealWithUnderflow(){
+            return new LinkedList.Node(1);
+        }
+        
+        // creating a method DealWithOverflow which simply sets the head to null and data to 0. therefore emptying the list down to the head
+        void DealWithOverflow() {
+            data = 0;
+            next = null;
+        }
 
     }
 }
